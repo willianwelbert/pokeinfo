091a9afb0b9ac2526c54c5f13f6a823412c32a8b
willianwelbert <willian.w.s@hotmail.com>
2018-03-18 22:29:45 -0300

    small adjusts for a better skills visualization

--- pokemons.js
+++ pokemons.js
@@ -37,7 +37,7 @@ function createCard () {
     <div class="pokemon-info">
         <h1 class="name">${pokemon.name}</h1>
         <font class="number">Nº ${pokemon.id} - Type: ${pokemon.types.map(item => item.type.name).toString()}</font>
-        <h3 class="skill">Skills: ${pokemon.moves.map(item => ' ' + item.move.name).toString()}</h3>
+        <p class="skill">Skills: ${pokemon.moves.map(item => ' ' + item.move.name).toString()}</p>
         <h3 class="weight">Weight: ${pokemon.weight  / 10}kg</h3>
         <h3 class="height">Height: ${pokemon.height  / 10}m</h3>
     </div>`;
